13fd47903f8658a15d0ae1c65fce3754b904be25
Ticxo <41558177+Ticxo@users.noreply.github.com>
2021-05-26 05:43:37 +0800

    Bone options compile & parse

--- meg.js
+++ meg.js
@@ -1,3 +1,13 @@
+var modelEngineOptions = {};
+
+var compileCallback = (e) => {
+    e.model.meg = modelEngineOptions;
+};
+
+var parseCallback = (e) => {
+    Object.assign(modelEngineOptions, e.model.meg);
+};
+
 (function() {
     Plugin.register('meg', {
         title: 'ModelEngine',
@@ -7,6 +17,8 @@
         version: '0.1.0',
         variant: 'both',
         onload() {
+            Codecs.project.on('compile', compileCallback);
+            Codecs.project.on('parse', parseCallback);
             Blockbench.showQuickMessage("MEG Loaded!", 2000);
 
             editAction = new Action('meg_bone_options', {
@@ -18,11 +30,13 @@
 					setBoneTypeMenu().show();
 				}
 			})
+			Group.prototype.menu.structure.push('_');
             Group.prototype.menu.addAction(editAction)
 		},
 
 		onunload() {
 			this.onuninstall();
+			Codecs.project.events.compile.remove(compileCallback);
 		},
 		onuninstall() {
             // Prevent action flood when reloading the plugin, remove once pushed to the public
@@ -31,20 +45,43 @@
 	})
 })();
 
-
 function setBoneTypeMenu(){
-    
+
+    var op = modelEngineOptions[Group.selected.uuid];
+    function getHead() {
+        if(op)
+            return op.is_head;
+        return false;
+    }
+    function getMount() {
+        if(op)
+            return op.is_mount;
+        return false;
+    }
+    function getHand() {
+        if(op)
+            return op.hand;
+        return 'none';
+    }
+    function getExtra() {
+        if(op)
+            return op.extra;
+        return '';
+    }
+
     var boneTypeDialog = new Dialog({
         id: 'bone_option_dialog',
         title: 'Bone Options',
         form: {
             isHead: {
                 label: 'Head',
-                type: 'checkbox'
+                type: 'checkbox',
+                value: getHead()
             },
             isMount: {
                 label: 'Mount',
-                type: 'checkbox'
+                type: 'checkbox',
+                value: getMount()
             },
             isHand: {
                 label: 'Hand',
@@ -53,17 +90,33 @@ function setBoneTypeMenu(){
                     none: 'Not Hand',
                     left: 'Left',
                     right: 'Right'
-                }
+                },
+                value: getHand()
             },
             extraOptions: {
                 label: 'Extra',
                 type: 'textarea',
-                placeholder: 'option1=value1\noption2=value2'
+                placeholder: 'option1=value1\noption2=value2\n...',
+                value: getExtra()
             }
         },
 		onConfirm: function(formData) {
 			// Do something when confirm
             // func(formData.isHead, formData.isHand, ...)
+            var op = modelEngineOptions[Group.selected.uuid];
+            if(op) {
+                op.is_head = formData.isHead;
+                op.is_mount = formData.isMount;
+                op.hand = formData.isHand;
+                op.extra = formData.extraOptions;
+            }else {
+                modelEngineOptions[Group.selected.uuid] = {
+                    is_head: formData.isHead,
+                    is_mount: formData.isMount,
+                    hand: formData.isHand,
+                    extra: formData.extraOptions
+                };
+            }
 			this.hide();
 		},
 		onCancel: function(formData) {
